e7789eb1566183f0eb4f5bd3f3d2155ae1208ee7
Bibin Graceson J <118437373+bibinwithab@users.noreply.github.com>
2024-01-05 13:25:27 +0530

    build: database connected

--- server.js
+++ server.js
@@ -1,37 +1,42 @@
+require("dotenv").config();
 const express = require("express");
 const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 3000;
-const { logger } = require('./middleware/logger')
-const { errorHandler } = require('./middleware/errorHandler')
-const cookieParser = require("cookie-parser")
-const cors = require("cors")
+const { logger } = require("./middleware/logger");
+const { errorHandler } = require("./middleware/errorHandler");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+const mongoose = require("mongoose");
 
-app.use(cookieParser())
+app.use(cookieParser());
 
-app.use(cors())
+app.use(cors());
 
-app.use(logger)
+app.use(logger);
 
 app.use(express.json());
 
-app.use('/', express.static(path.join(__dirname, '/public')))
+app.use("/", express.static(path.join(__dirname, "/public")));
 
-app.use('/', require('./routes/root'))
+app.use("/", require("./routes/root"));
 
-app.all('*', (req, res)=>{
-    res.status(404)
-    if (req.accepts('html')) {
-        res.sendFile(__dirname, 'views', '404.html')
-    }else if (req.accepts('json')) {
-        res.json({message: '404 Not found'})
-    }else {
-        res.type('txt').send('404 Not found')
-    }
-})
+app.all("*", (req, res) => {
+  res.status(404);
+  if (req.accepts("html")) {
+    res.sendFile(__dirname, "views", "404.html");
+  } else if (req.accepts("json")) {
+    res.json({ message: "404 Not found" });
+  } else {
+    res.type("txt").send("404 Not found");
+  }
+});
 
-app.use(errorHandler)
+app.use(errorHandler);
 
 app.listen(PORT, () => {
   console.log(`server up on http://localhost:${PORT}`);
+  mongoose.connect(process.env.DATABASE_URI).then(() => {
+    console.log("connected to db");
+  });
 });
